aada8761b7a6b0187a784d11cf55fa59cb8c7781
Gustavo <whoisgustavolima@gmail.com>
2026-01-12 20:30:32 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import Home from './pages/home/page.jsx'
-import MentoriaIA from './pages/mentoria/ia/page.jsx'
-import MentoriaJovana from './pages/mentoria/gestao-de-escritorio/page.jsx'
+import MentoriaIA from './pages/inteligencia-artificial/page.jsx'
+import MentoriaJovana from './pages/gestao-de-escritorio/page.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mentoria/inteligencia-artificial" element={<MentoriaIA />} />
-        <Route path="/mentoria/gestao-de-escritorio" element={<MentoriaJovana />} />
+        <Route path="/inteligencia-artificial" element={<MentoriaIA />} />
+        <Route path="/gestao-de-escritorio" element={<MentoriaJovana />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
